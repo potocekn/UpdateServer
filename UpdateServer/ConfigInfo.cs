a4d95c51a8d30fd40bb980e9851293789bc5e721
Natalia Potocekova <natalia.potocekova@yahoo.com>
2021-02-02 18:22:21 +0100

    added class to store config information

---
+++ UpdateServer/ConfigInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateServer
+{
+    class ConfigInfo
+    {
+        public string AvailableResourcesCheckExeLocation { get; set; }
+        public string ConfigInfoForScriptLocation { get; set; }
+        public string JsonFilesFolder { get; set; }
+        public string WhereToSaveChangedResources{ get; set; }
+               
+    }
+}

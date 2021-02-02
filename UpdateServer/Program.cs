874fb68b73f7175ef90d0493c8c450b5fd2be76f
Natalia Potocekova <natalia.potocekova@yahoo.com>
2021-02-02 14:01:39 +0100

    starts available resources check script with given parameters

--- UpdateServer/Program.cs
+++ UpdateServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace UpdateServer
 {
@@ -6,7 +7,27 @@ namespace UpdateServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+
+                Process firstProc = new Process();
+                firstProc.StartInfo.FileName = @"C:\Users\User\source\repos\AvailableResourcesCheck\AvailableResourcesCheck\bin\Debug\netcoreapp3.1\AvailableResourcesCheck.exe";
+                firstProc.StartInfo.Arguments = @"C:\Users\User\Desktop\rp_folders\config\config_info.txt C:\Users\User\Desktop\rp_folders\json_test\ C:\Users\User\Desktop\rp_folders\changes\changes.txt";
+                firstProc.EnableRaisingEvents = true;
+
+                firstProc.Start();
+
+                firstProc.WaitForExit();
+
+                //You may want to perform different actions depending on the exit code.
+                Console.WriteLine("First process exited: " + firstProc.ExitCode);               
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred!!!: " + ex.Message);
+                return;
+            }
         }
     }
 }

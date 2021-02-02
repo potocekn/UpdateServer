d7e56fa7fa6cc021fbd7476ee91851562c7d2835
Natalia Potocekova <natalia.potocekova@yahoo.com>
2021-02-02 18:33:56 +0100

    minor changes

--- UpdateServer/Program.cs
+++ UpdateServer/Program.cs
@@ -11,23 +11,21 @@ namespace UpdateServer
             try
             {
                 ConfigInfo configInfo = Parser.Parse(@"C:\Users\User\Desktop\rp_folders\config\config_info_server.txt");
-                Process firstProc = new Process();
-                firstProc.StartInfo.FileName = configInfo.AvailableResourcesCheckExeLocation;
+                Process recourceCheckProcess = new Process();
+                recourceCheckProcess.StartInfo.FileName = configInfo.AvailableResourcesCheckExeLocation;
                 StringBuilder arguments = new StringBuilder();
                 arguments.Append(configInfo.ConfigInfoForScriptLocation);
                 arguments.Append(" ");
                 arguments.Append(configInfo.JsonFilesFolder);
                 arguments.Append(" ");
                 arguments.Append(configInfo.WhereToSaveChangedResources);
-                firstProc.StartInfo.Arguments = arguments.ToString();
-                firstProc.EnableRaisingEvents = true;
+                recourceCheckProcess.StartInfo.Arguments = arguments.ToString();
+                recourceCheckProcess.EnableRaisingEvents = true;
 
-                firstProc.Start();
-
-                firstProc.WaitForExit();
-
-                //You may want to perform different actions depending on the exit code.
-                Console.WriteLine("First process exited: " + firstProc.ExitCode);
+                recourceCheckProcess.Start();
+                recourceCheckProcess.WaitForExit();
+                                
+                Console.WriteLine("Resource check process exited: " + recourceCheckProcess.ExitCode);
 
             }
             catch (InvalidParamInConfigFileException ex)

a6d5421ec0223375c186b9169aebc49cb743473b
Natalia Potocekova <natalia.potocekova@yahoo.com>
2021-02-02 18:21:59 +0100

    added special exception for invalid parameter in config file

---
+++ UpdateServer/InvalidParamInConfigFileException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateServer
+{
+    class InvalidParamInConfigFileException: Exception
+    {
+        public InvalidParamInConfigFileException(){}
+
+        public InvalidParamInConfigFileException(string name): base(String.Format("Invalid parameter in config file: {0}", name)){}
+    }
+}

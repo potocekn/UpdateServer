29b4fbebb124763466243764898bd9f2996593be
Natalia Potocekova <natalia.potocekova@yahoo.com>
2021-02-09 19:50:29 +0100

    added classes for requests

---
+++ UpdateServer/Request.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateServer
+{
+    class Request
+    {
+        public RequestType Type { get; }
+        public List<string> Params { get; }
+
+        public Request(RequestType type, List<string> parameters) 
+        {
+            Type = type;
+            Params = parameters;
+        }
+    }
+}
